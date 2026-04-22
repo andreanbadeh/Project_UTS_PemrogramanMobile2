f78662939019f71328fb571fb653e84a103edbda
andreanputraarya <168192819+andreanbadeh@users.noreply.github.com>
2026-04-22 11:25:02 +0700

    Add files via upload

---
+++ java/com.example.com/events/EventsActivity.java
@@ -0,0 +1,15 @@
+package com.example.notes.events;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.notes.R;
+
+public class EventsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_events);
+    }
+}
